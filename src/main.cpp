6dc53770613f6ed768d489af902b661a1d84f78c
JeetChauhan17 <chauhanjj1999@gmail.com>
2024-09-08 00:40:51 +0530

    starting lexical tokenisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include<iostream>
 #include<fstream>
 #include<sstream>
+#include<optional>
+#include<vector>
+
+// Lexical tokenisation starts here. 
+// Enumeration: 
+enum class TokenType{
+    jreturn,
+    int_lit,
+    jemi
+};
+
+struct Token{
+    TokenType type;
+    std::optional<std::string> value;
+};
+
+
+// remember soldier, switching to  a vector is faster tha reloading
+// personal hate to list
+
+std::vector<Token> tokenize (const std::string& str){
+    // iterate through each word and tokenise it. assuming its ascii lol
+    for(char c : str){
+        std::cout<< c << std::endl;
+    }
+
+    // 3rd approah: if i see a letter, i'll start readig it into a buffer until its no loger a letter or no,
+    // that'll maybe help find out what the buffer contains and it its a token.
+
+    // todo : fix segfault (add ret whe i have cotent worth returing)
+}
 
 int main(int argc, char* argv[]){
     // dont let the arg to be 0 or 1:
@@ -22,7 +53,8 @@ int main(int argc, char* argv[]){
 
 
 
-    std::cout<<contents<<std::endl; //works heheheha
+    // std::cout<<contents<<std::endl; //works heheheha
+    tokenize(contents);
     return EXIT_SUCCESS;
     
 }
